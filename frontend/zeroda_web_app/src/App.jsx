40443c66802785c9ac93648f1572005203edf236
Tahil40 <www.mohd.tahilnazir9216@gmail.com>
2026-06-30 23:20:19 +0530

    start_configuring_react_router

--- frontend/zeroda_web_app/src/App.jsx
+++ frontend/zeroda_web_app/src/App.jsx
@@ -1,12 +1,31 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomeLayout from "./LandingPage/Home/HomeLayout";
+import AboutLayoutSection from "./LandingPage/About/AboutLayout";
+import PricingLayout from "./LandingPage/Pricing/PricingLayout";
+import ProductLayout from "./LandingPage/Products/ProductLayout";
+import SignUp from "./LandingPage/SignUp/SignUp";
+import SupportLayout from "./LandingPage/Support/SupportLayout";
+import Navbar from "./LandingPage/components/Navbar";
+import Footer from "./LandingPage/components/Footer";
 
 function App() {
-
   return (
-    <>
-    
-    </>
-  )
+    <BrowserRouter>
+    {/* adding navbar component */}
+    <Navbar/>
+    <Routes>
+      <Route path="/" element={<HomeLayout/>}/>
+      <Route path="/about" element={<AboutLayoutSection/>}/>
+      <Route path="/pricing" element={<PricingLayout/>}/>
+      <Route path="/products" element={<ProductLayout/>}/>
+      <Route path="/signup" element={<SignUp/>}/>
+      <Route path="/support" element={<SupportLayout/>}/>
+    </Routes>
+    {/* adding footer component */}
+    <Footer/>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- frontend/zeroda_web_app/src/main.jsx
+++ frontend/zeroda_web_app/src/main.jsx
@@ -1,12 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-// import App from './App.jsx'
-import HomeLayout from './LandingPage/Home/HomeLayout'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <App /> */}
-    <HomeLayout/>
+    <App />
   </StrictMode>,
 )
