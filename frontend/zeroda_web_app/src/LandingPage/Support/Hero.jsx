7a3599d0406b1dc307acd5dcc3b375ede52e61eb
Tahil40 <www.mohd.tahilnazir9216@gmail.com>
2026-07-01 23:24:47 +0530

    start_configuring_support_page

--- frontend/zeroda_web_app/src/LandingPage/Support/Hero.jsx
+++ frontend/zeroda_web_app/src/LandingPage/Support/Hero.jsx
@@ -1,11 +1,31 @@
 import React from "react";
 
 const Hero = () => {
-    return(
-        <>
-        <h1>Hero</h1>
-        </>
-    );
+  return (
+    <>
+      <div className="bg-primary p-5 text-white" style={{ marginTop: "4.8rem" }}>
+        <div className="container">
+          <div className="row bg-primary">
+            <div className="col-6">
+                <h1 className="fs-5">Support Portal</h1>
+                <p>Search for an answer or browse help topics <br />to create a ticket</p>
+                <input type="text" name="" id="" />
+                <a href=""></a><a href=""></a><a href=""></a>
+                <a href=""></a><a href=""></a>
+            </div>
+            <div className="col-6">
+                <h1 className="fs-5">Track Tickets</h1>
+                <p>Featured</p>
+                <ol type="1">
+                    <li><a href=""></a></li>
+                    <li><a href=""></a></li>
+                </ol>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Hero;
